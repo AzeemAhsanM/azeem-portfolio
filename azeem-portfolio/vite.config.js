d9c2a5c6c2c6ccfc6841dc210e599e70a09dc2ed
AzeemAhsanM <azeemahsan67@gmail.com>
2026-01-15 13:24:25 +0530

    first commit

--- azeem-portfolio/vite.config.js
+++ azeem-portfolio/vite.config.js
@@ -1,4 +1,8 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/azeem-portfolio/', // Replace with your actual repository name
+  base: '/azeem-portfolio/', 
 })
